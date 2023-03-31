51785bedf21ca798503d4919bbb310282854f6e7
pestich <pestich@mail.ru>
2023-03-31 22:27:31 +0300

    Lesson 36 (Goals)

--- todolist/goals/serializers.py
+++ todolist/goals/serializers.py
@@ -1,7 +1,10 @@
+from datetime import date
+
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError, PermissionDenied
 
 from todolist.core.serializers import ProfileSerializer
-from todolist.goals.models import GoalCategory
+from todolist.goals.models import GoalCategory, Goal
 
 
 class GoalCategoryCreateSerializer(serializers.ModelSerializer):
@@ -20,3 +23,38 @@ class GoalCategorySerializer(serializers.ModelSerializer):
         model = GoalCategory
         read_only_fields = ('id', 'created', 'updated', 'user', 'is_deleted')
         fields = '__all__'
+
+
+class GoalCreateSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
+    class Meta:
+        model = Goal
+        read_only_fields = ('id', 'created', 'updated', 'user')
+        fields = '__all__'
+
+    def validate_category(self, value: GoalCategory):
+        if value.is_deleted:
+            raise ValidationError('Category not found')
+        if self.context['request'].user.id != value.user_id:
+            raise PermissionDenied
+        return value
+
+    # def validate_due_date(self, value: date):
+    #
+
+
+class GoalSerializer(serializers.ModelSerializer):
+    user = ProfileSerializer(read_only=True)
+
+    class Meta:
+        model = Goal
+        read_only_fields = ('id', 'created', 'updated', 'user')
+        fields = '__all__'
+
+    def validate_category(self, value: GoalCategory):
+        if value.is_deleted:
+            raise ValidationError('Category not found')
+        if self.context['request'].user.id != value.user_id:
+            raise PermissionDenied
+        return value
